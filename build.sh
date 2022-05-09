e5123bab9b03097c935e62d6f0d9c7ab667b5cdd
Noriyuki Abe <noriyukipy@gmail.com>
2022-05-10 08:29:40 +0900

    Build

--- build.sh
+++ build.sh
@@ -2,5 +2,14 @@ set -eu
 
 target=jupyter_book
 output=$(pwd)/docs
-jb build ${target}/
+
+# Clean up build folder
+jupyter-book clean -a ${target}/
+jupyter-book build ${target}/
+
+# Clean up docs folder
+[ -e "${output}" ] && rm -r ${output} && mkdir ${output}
 cp -r ${target}/_build/html/* ${output}/
+# 次の行を実行しないとGithub Pagesがjekyllだと認識して表示しようとするため
+# CSSのPATH等が正しく認識されずに描画に失敗するので注意
+touch ${output}/.nojekyll
